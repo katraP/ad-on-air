045d5aabbeb01f47bb4836b015f16e8f1afca401
Kateryna_Porkhun <kateryna_porkhun@epam.com>
2016-04-25 17:27:55 +0300

    ..

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,12 +2,16 @@
  * Created by Kateryna_Porkhun on 4/24/2016.
  */
 $(function() {
-    $(window).on('scroll', function() {
-        if($(window).scrollTop() >= 150) {
-            $('.ad-header').addClass('scrolled');
-        }
-        else {
-            $('.ad-header').removeClass('scrolled');
-        }
-    });
+    //$(window).on('scroll', function() {
+    //    if($(window).scrollTop() >= 150) {
+    //        $('.ad-header').addClass('scrolled');
+    //    }
+    //    else {
+    //        $('.ad-header').removeClass('scrolled');
+    //    }
+    //});
+
+    var id = setTimeout(function() {
+        alert('timeout!');
+    }, 10000)
 });
