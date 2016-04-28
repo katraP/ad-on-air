c1c2357763051242b11640b9e3a8dc7b254d7117
Kateryna_Porkhun <kateryna_porkhun@epam.com>
2016-04-28 12:48:20 +0300

    ..

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,22 +2,36 @@
  * Created by Kateryna_Porkhun on 4/24/2016.
  */
 $(function() {
+    var anchorMap = {
+        l1: 'banner',
+        l2: 'media',
+        l3: 'contact',
+        l4: 'team',
+        l5: 'event'
+    }
+
     var media = '.ad-media';
     var map = $('.ad-contact');
     var currentBannerTranslate = 0;
     var banner = $('.ad-banner__bg');
+    var menuLink = $('.ad-nav__link');
+    var contactFormBut = $('.ad-contact__button');
+
     $(window).on('scroll', function() {
+        menuScroll();
+        bannerScroll();
+        showMedia();
+        showMap();
+    });
+
+    function menuScroll() {
         if($(window).scrollTop() >= 90) {
             $('body').addClass('scrolled');
         }
         else {
             $('body').removeClass('scrolled');
         }
-        bannerScroll();
-        showMedia();
-        showMap();
-    });
-
+    }
     function bannerScroll() {
         currentBannerTranslate = $(window).scrollTop()/3;
         banner.css('transform', 'translate(0,'+currentBannerTranslate+'px)')
@@ -33,6 +47,31 @@ $(function() {
             map.addClass('animated');
         }
     }
+    function animateMenu(event, el) {
+        event.preventDefault();
+        var element = $(el);
+        var type = element.attr('class').split(' ')[1];
+        if(type === 'l1') {
+            var offsetTop = 0;
+        }
+        else {
+            var offsetTop = $('.ad-'+anchorMap[type]).offset().top - 40;
+        }
+        $('html, body').animate({
+            scrollTop: offsetTop
+        }, 600);
+    }
+    function submitForm(e) {
+        e.preventDefault();
+        contactFormBut.addClass('ad-contact__button--clicked').val('Success!');
+    }
+
+
+    menuLink.on('click', function(e) {
+        animateMenu(e, this);
+    });
+    contactFormBut.on('click', submitForm);
+    menuScroll();
     showMedia();
     showMap();
 });
